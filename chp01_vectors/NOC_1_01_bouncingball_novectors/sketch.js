4a63bf5c26b63bfc2aa53ba60edb5e2b445e07d3
uto-usui <me@uto-usui.com>
2020-01-23 12:12:32 +0900

    ✨ Update 1_01

--- chp01_vectors/NOC_1_01_bouncingball_novectors/sketch.js
+++ chp01_vectors/NOC_1_01_bouncingball_novectors/sketch.js
@@ -7,28 +7,27 @@ let x = 100;
 let y = 100;
 let xspeed = 2.5;
 let yspeed = 2;
+let size = 48;
 
 function setup() {
   createCanvas(640, 360);
 }
 
 function draw() {
-  background(51);
+  background(10);
 
   // Add the current speed to the position.
   x = x + xspeed;
   y = y + yspeed;
 
-  if ((x > width) || (x < 0)) {
+  if ((x > width - size / 2) || (x - size / 2 < 0)) {
     xspeed = xspeed * -1;
   }
-  if ((y > height) || (y < 0)) {
+  if ((y > height - size / 2) || (y - size / 2 < 0)) {
     yspeed = yspeed * -1;
   }
 
   // Display circle at x position
-  stroke(0);
-  strokeWeight(2);
-  fill(127);
-  ellipse(x, y, 48, 48);
+  stroke(255);
+  ellipse(x, y, size, size);
 }
